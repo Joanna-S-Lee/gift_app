18937f0519db865aa0a821128558c3710a155e28
Joanna-S-Lee <annaoj91@gmail.com>
2021-06-14 19:40:39 +1000

    added comments to gifts controller

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -2,35 +2,30 @@ class GiftsController < ApplicationController
   before_action :set_gift, only: [:show, :edit, :update, :destroy]
   before_action :authorize_gift, only: [:edit, :update, :destroy]
 
-  # GET /gifts
-  # def index
-  #   @gifts = Gift.all
-  # end
 
   def index
     if params[:query].present?
+      # searches the Gifts table for entries that have the query as the name attribute
       @gifts = Gift.search_by_name(params[:query])
     else
+      # otherwise it returns all the Gifts from the Gifts table.
       @gifts = Gift.all
     end
   end
-
-  # GET /gifts/1
+ 
   def show
   end
-
-  # GET /gifts/new
+  
   def new
     @gift = Gift.new
   end
-
-  # GET /gifts/1/edit
+ 
   def edit
     
   end
-
-    # POST /gifts
+   
   def create
+    # This attempts to create a new Gift
     @gift = Gift.new(gift_params)
     @gift.user_id = current_user.id
 
@@ -40,25 +35,27 @@ class GiftsController < ApplicationController
       render :new
     end
   end
-
-  # PATCH/PUT /gifts/1
+ 
   def update
+    # This attempts to update an existing Gift
     if @gift.update(gift_params)
       redirect_to @gift, notice: 'Gift was successfully updated.'
     else
       render :edit
     end
   end
-
-  # DELETE /gifts/1
+ 
   def destroy
+    # This deletes an existing Gift
     @gift.destroy
     redirect_to gifts_url, notice: 'Gift was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
+
     def set_gift
+      # Returns the Gift that has the primary key requested in the params
       @gift = Gift.find(params[:id])
     end
 
